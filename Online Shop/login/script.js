991a948e1a0812dcac89fb3a749872cf80529781
MoazAzmy <moaz.ahmed.azmy@gmail.com>
2025-08-17 19:09:44 +0300

    Login js

---
+++ Online Shop/login/script.js
@@ -0,0 +1,15 @@
+function login(event) {
+  var email = document.getElementById('email').value;
+  var password = document.getElementById('password').value;
+
+  if(email === '' || password === '') {
+    event.preventDefault();
+    alert('Please fill in all fields.');
+    return false;
+  }
+  if(email === 'admin@gmail.com' && password === 'admin') {
+    event.preventDefault();
+    window.location.href = '/admin/Index.html';
+    return false;
+  }
+}
